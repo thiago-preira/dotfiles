12a76b0bf951fa5e6c8c0367b0b0c57ca5f8bcfa
Thiago Pereira <2319062+thiago-preira@users.noreply.github.com>
2022-12-12 15:04:33 +0000

    Dotfiles empty machine

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,12 @@
+# Alias
+# ---
+#
+alias k="kubectl"
+
+# ALIAS COMMANDS
+alias grep='grep --color'
+alias reload='source ~/.zshrc'
+alias svim='sudo vim'
+alias timestamp="date +'%Y%m%d%H%M%S'"
+alias gac="git add . && git commit -a -m "
+alias update='sudo apt-get update && sudo apt-get upgrade'
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# dotfiles
-dotfiles repo
+# Dotfiles
+
+Dotfiles for linux machine
+
+Uses ZSH as default prompt
+Uses [starship](https://starship.rs) to configure prompt
+Has git configuration
+Environment configuration
+
